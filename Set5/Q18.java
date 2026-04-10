dce62baae9e7896d6bc03ccdf69c887efd2088e4
Krupa_195 <256949786+krupavaghasiya09-netizen@users.noreply.github.com>
2026-04-10 21:50:33 +0530

    Q18_Implement Vehicle hierarchy with Car and Bike classes

---
+++ Set5/Q18.java
@@ -0,0 +1,41 @@
+abstract class Vehicle {
+    
+    abstract String fuelType();
+    abstract int noOfWheels();
+}
+
+class Car extends Vehicle {
+    
+    String fuelType() {
+        return "Petrol/Diesel";
+    }
+    
+    int noOfWheels() {
+        return 4;
+    }
+}
+
+class Bike extends Vehicle {
+    
+    String fuelType() {
+        return "Petrol";
+    }
+    
+    int noOfWheels() {
+        return 2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Vehicle car = new Car();
+        Vehicle bike = new Bike();
+        
+        System.out.println("Car Fuel: " + car.fuelType());
+        System.out.println("Car Wheels: " + car.noOfWheels());
+        
+        System.out.println("Bike Fuel: " + bike.fuelType());
+        System.out.println("Bike Wheels: " + bike.noOfWheels());
+    }
+}
