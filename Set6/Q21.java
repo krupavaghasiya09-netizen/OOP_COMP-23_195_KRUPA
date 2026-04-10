76ec5ce0e1ef9d79acd7952f650d9b7a0d26a11d
Krupa_195 <256949786+krupavaghasiya09-netizen@users.noreply.github.com>
2026-04-10 22:31:13 +0530

    Q21_Add VotingApp to check voting eligibility
    Implement voting eligibility check based on age.

---
+++ Set6/Q21.java
@@ -0,0 +1,31 @@
+class VotingApp {
+
+       void checkEligibility(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be 18 or above to vote");
+        } else {
+            System.out.println("You are eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        VotingApp v = new VotingApp();
+
+        int[] ages = {16, 28, 10, 25}; 
+
+        for (int age : ages) {
+            try {
+                System.out.println("Checking age: " + age);
+                v.checkEligibility(age);
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+            finally {
+                System.out.println("Validation process completed");
+                System.out.println();
+            }
+        }
+    }
+}
