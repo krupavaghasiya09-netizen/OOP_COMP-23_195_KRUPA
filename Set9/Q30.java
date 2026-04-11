c8e0f42f92fe9e69b7de6f025cd4156b11a41a5d
Krupa_195 <256949786+krupavaghasiya09-netizen@users.noreply.github.com>
2026-04-11 21:44:13 +0530

    Add WordFrequency class to count word occurrences

---
+++ Set9/Q30.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String input = sc.nextLine();
+
+      
+        String[] words = input.trim().split("\\s+");
+
+      
+        HashMap<String, Integer> map = new HashMap<>();
+
+     
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+
+     
+        System.out.println("\nWord Frequencies:");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        sc.close();
+    }
+}
